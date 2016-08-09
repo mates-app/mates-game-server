290ea49a47b191fbca9aecee9ceb6945b75dd582
josecullen <jose_cullen@hotmail.com>
2016-08-09 00:33:33 -0300

    Se agrega endpoint para crear problemas (solo simples)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 var matesEngine = require('./mates-engine-connection')
 var db = require('./database')
-
+let q = require('q')
 
 
 
@@ -49,10 +49,79 @@ router.post('/v1/admin/game-config', (req, res, next) => {
 
 router.get('/v1/admin/game-config/:id', (req, res, next) => {
   console.log('request', req.params.id)
+
+
   db.getGameConfig(req.params.id)
-    .then((gameConfig) => res.send(gameConfig))
+    .then((gameConfig) => {
+      createGame(gameConfig).then((game) => res.send(game))
+    })
 })
 
+
+function createGame(gameConfig){
+  let deferred = q.defer()
+
+  let game = {
+    "name" : gameConfig.name,
+    "levels": []
+  }
+
+  gameConfig.levels.forEach((level, index, array) =>{
+    console.log('level', index, array.length)
+
+    createGameProblems(level.gameProblems).then((gameProblems) =>{
+      console.log('forEach createGameProblem')
+      
+      game.levels.push({
+        "name" : level.name,
+        "gameProblems" : gameProblems,
+        "scoreConfig" : level.scoreConfig
+      })
+
+      if(index === array.length -1){
+        console.log('resolve')
+        deferred.resolve(game)
+      }
+    })
+
+  })
+
+  return deferred.promise
+}
+
+function createGameProblems(gameConfigs){
+  let deferred = q.defer()
+
+  let gameProblems = []
+
+  gameConfigs.forEach((gameConfig, index, array) =>{
+    createGameProblem(gameConfig).then((gameProblem) =>{
+      gameProblems.push(gameProblem)
+      if(index === array.length -1){
+        console.log('resolve gameConfigs')
+        deferred.resolve(gameProblems)
+      }
+    })
+  })
+
+  return deferred.promise
+}
+
+
+function createGameProblem(gameConfig){
+  let deferred = q.defer()
+  
+  console.log('createGameProblem', gameConfig)
+
+  if(gameConfig.type === 'simple-problem'){
+    console.log('simple-problem', gameConfig)
+    matesEngine.simpleProblem(gameConfig).then((gameProblem) => {
+      deferred.resolve(gameProblem)
+    })
+  }
+
+  return deferred.promise  
+}
 
 
 module.exports = router;
